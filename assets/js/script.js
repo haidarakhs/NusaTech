d7dd8e87c44875450417e142d46de1f9be768b96
haidarakhs <haidarakbar.hs@gmail.com>
2025-01-17 10:01:46 +0700

    update new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,27 @@
 "use strict";
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
-    }
-  } else {
-    elem.addEventListener(type, callback);
-  }
+const addEventOnElem = (elements, eventType, callback) => {
+  elements.forEach((elem) => elem.addEventListener(eventType, callback));
 };
 
 /**
- * Navbar toggle functionality.
+ * Navbar
  */
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const navLinks = document.querySelectorAll("[data-nav-link]");
 const overlay = document.querySelector("[data-overlay]");
 
-const toggleNavbar = function () {
+const toggleNavbar = () => {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
 };
-
 addEventOnElem(navTogglers, "click", toggleNavbar);
-
-const closeNavbar = function () {
+const closeNavbar = () => {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
 };
-
 addEventOnElem(navLinks, "click", closeNavbar);
 
-/**
- * Header and back-to-top button visibility on scroll.
- */
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
@@ -46,68 +34,38 @@ const handleScroll = () => {
 window.addEventListener("scroll", debounce(handleScroll, 100));
 
 /**
- * Video play/pause functionality.
+ * Video Play/Pause 
  */
 const playButton = document.querySelector(".play-btn");
 const video = document.querySelector("video");
 
+const togglePlayButtonVisibility = () => {
+  playButton.classList.toggle("hidden", !video.paused);
+};
+
 playButton.addEventListener("click", () => {
   if (video.paused) {
     video.play();
-    playButton.classList.add("hidden");
   } else {
     video.pause();
-    playButton.classList.remove("hidden");
   }
 });
 
-video.addEventListener("play", () => playButton.classList.add("hidden"));
-video.addEventListener("pause", () => playButton.classList.remove("hidden"));
-video.addEventListener("ended", () => playButton.classList.remove("hidden"));
+video.addEventListener("play", togglePlayButtonVisibility);
+video.addEventListener("pause", togglePlayButtonVisibility);
+video.addEventListener("ended", togglePlayButtonVisibility);
 
 /**
- * Carousel functionality using jQuery.
+ * Carousel 
  */
-jQuery(document).ready(function ($) {
-  $(".slider-img").on("click", function () {
-    $(".slider-img").removeClass("active");
-    $(this).addClass("active");
+const sliderImages = document.querySelectorAll(".slider-img");
+
+sliderImages.forEach((img) => {
+  img.addEventListener("click", () => {
+    sliderImages.forEach((el) => el.classList.remove("active"));
+    img.classList.add("active");
   });
 });
-
-/**
- * Animate elements 
- */
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("animate__animated", "animate__fadeInUp");
-      } else {
-        entry.target.classList.remove("animate__animated", "animate__fadeInUp");
-      }
-    });
-  },
-  { threshold: 0.1 } 
-);
-
-document.querySelectorAll(".animate-on-scroll").forEach((el) => {
-  observer.observe(el);
-});
-
-
-
-
-const handleAboutScroll = () => {
-  const scrollY = window.scrollY;
-
-  aboutContent.classList.toggle("show", scrollY > 100);
-  aboutImage.classList.toggle("show", scrollY > 100);
-  scrollUpBtn.style.display = scrollY > 200 ? "block" : "none";
-};
-
-window.addEventListener("scroll", debounce(handleAboutScroll, 100));
-
 
 function debounce(func, wait) {
   let timeout;
